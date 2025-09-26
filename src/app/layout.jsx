a7dbc380aa7636e8dc6e37f40803b30399786c40
tahab-cp <taha.b@cp.agency>
2025-09-26 18:03:28 +0500

    project updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Onest } from "next/font/google";
 import "../styles/globals.css";
+import Script from "next/script";
 
 const onest = Onest({
   subsets: ["latin"],
@@ -21,7 +22,33 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-B8FV6K69');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${onest.className} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-B8FV6K69"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager */}
+
         <main>{children}</main>
       </body>
     </html>
